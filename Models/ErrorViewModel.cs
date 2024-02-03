bc1f12cb40ae23dbfc5611f105f71027dc9129a1
ahmadmunir108 <ahmadmunir108@gmail.com>
2024-02-03 19:21:32 +0530

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace munirwebgitrepo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
